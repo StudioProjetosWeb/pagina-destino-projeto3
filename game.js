5f9dc9f866cd1480661e868a5eb3b5ba68b3f610
DIOGENES GOMES SANTOS DA SILVA <133171059+StudioProjetosWeb@users.noreply.github.com>
2023-11-09 01:08:09 -0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+// Este código precisa ser expandido para criar um jogo Pong totalmente funcional.
+const canvas = document.getElementById('pongCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 400;
+
+// Desenhe o jogo aqui, implementando lógica para raquetes, bola, colisões e pontuação.
+
+// A implementação completa do jogo Pong envolve vários aspectos, como lógica de jogo, detecção de colisão, pontuação, etc.
